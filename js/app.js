a0ba54ef0ea02cb2802fa178b2bc55b2133c17c7
micky rendon <elderendon@hotmail.es>
2022-10-14 22:04:36 -0300

    getting span values

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const month = document.querySelector('.month')
+const days = document.querySelector('.days')
+const hours = document.querySelector('.hours')
+const minutes = document.querySelector('.minutes')
+const seconds = document.querySelector('.seconds')
+
+// getting the value of any span
+month.children[0].innerText
+month.children[1].innerText
+
+days.children[0].innerText
+days.children[1].innerText
+
+hours.children[0].innerText
+hours.children[1].innerText
+
+minutes.children[0].innerText
+minutes.children[0].innerText
+
+seconds.children[1].innerText
+seconds.children[1].innerText
